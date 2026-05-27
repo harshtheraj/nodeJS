9b0bbdf86d07330cfa389600c814331afae45003
harshtheraj <harshtheraj@gmail.com>
2026-05-27 21:29:07 +0530

    part 2 modules in nodesjs

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -1,2 +1,10 @@
-console.log("hey it's node js");
-console.log("wi ll be learning node js");
+// console.log("hey it's node js");
+
+//const math =require('./math')
+// console.log("math value is :",math.addFn(1,2),math.subFn(2,1))
+
+// const {addFn,subFn}=require('./math')
+// console.log("math value is :",addFn(1,2),subFn(2,1))
+
+const math =require('./math')
+console.log("math value is :",math.add(1,2),math.sub(2,1))
